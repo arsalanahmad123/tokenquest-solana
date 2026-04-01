9b8d5c5608e6b1cc9eab204d69dacecc142c49b9
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2026-04-01 20:33:21 +0500

    changed url

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -191,8 +191,8 @@ const Index = () => {
 
     const solana = useMemo(() => {
         if (!token || !type || !userId || !window.TokenQuestSDK) return null;
-        const baseUrl = 'http://localhost:3000/api';
-        // const baseUrl = 'https://testbot.tokenquest.ca/api';
+        // const baseUrl = 'http://localhost:3000/api';
+        const baseUrl = 'https://testbot.tokenquest.ca/api';
         try {
             return new window.TokenQuestSDK.SolanaInstance({
                 config: {
